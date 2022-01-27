26acb1c4e4d238e7301b421248215cb2e838c706
Aviraj Singh Chauhan <avirajc.ug20.ece@nitp.ac.in>
2022-01-27 14:40:38 +0530

    Event Detail Screen

---
+++ app/src/main/java/android/example/donationapp/Activity/EventDetailActivity.java
@@ -0,0 +1,15 @@
+package android.example.donationapp.Activity;
+
+import android.example.donationapp.R;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_details);
+    }
+}
