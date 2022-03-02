a28066130e0f80172978849bd19fa545746b8210
Sujit Kumar Das <sanjib121kumardas@gmail.com>
2022-03-02 20:25:52 +0530

    Chat Functionality Implemented

---
+++ app/src/main/java/android/example/donationapp/Adapters/MessageAdapter.java
@@ -0,0 +1,102 @@
+package android.example.donationapp.Adapters;
+
+import android.content.Context;
+import android.example.donationapp.Model.MessageClass;
+import android.example.donationapp.R;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.viewHolder> {
+
+    int ITEM_SENT = 1;
+    int ITEM_RECEIVE = 2;
+
+    Context context;
+    ArrayList<MessageClass> usersList;
+
+    public MessageAdapter(ArrayList<MessageClass> usersList, Context context)
+    {
+        this.context = context;
+        this.usersList = usersList;
+        Log.e("Adapter UserList",usersList.toString());
+    }
+
+    @NonNull
+    @Override
+    public MessageAdapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        if(viewType == ITEM_SENT)
+        {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_sent_layout, parent, false);
+            return new viewHolder(view);
+        }
+        else
+        {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_recieve_layout, parent, false);
+            return new viewHolder(view);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.viewHolder holder, int position) {
+
+        String message = usersList.get(position).getMessage();
+
+        if(message.equals(""))
+        {
+            holder.messageView.setHeight(0);
+            holder.messageView.setWidth(0);
+            holder.messageView.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            if(message != null)
+            holder.messageView.setText(message);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersList.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+
+        TextView messageView;
+
+        public viewHolder(@NonNull View itemView) {
+
+            super(itemView);
+
+            messageView = itemView.findViewById(R.id.senderMessage);
+
+        }
+
+
+    }
+
+    public int getItemViewType(int position) {
+        MessageClass message = usersList.get(position);
+
+        if (Objects.requireNonNull(FirebaseAuth.getInstance().getUid()).equalsIgnoreCase(message.getSenderId())) {
+            return ITEM_SENT;
+        } else {
+            return ITEM_RECEIVE;
+        }
+    }
+
+
+}
--- app/src/main/java/android/example/donationapp/Fragment/ChatFragment.java
+++ app/src/main/java/android/example/donationapp/Fragment/ChatFragment.java
@@ -1,12 +1,17 @@
 package android.example.donationapp.Fragment;
 
 import android.example.donationapp.Adapters.ChatAdapter;
+import android.example.donationapp.Adapters.UsersAdapter;
 import android.example.donationapp.Model.ChatClass;
+import android.example.donationapp.Model.UserClass;
+import android.example.donationapp.Model.UsersAdapterClass;
 import android.example.donationapp.R;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,12 +20,27 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 import java.util.concurrent.RecursiveAction;
 
 public class ChatFragment extends Fragment {
 
-    private ArrayList<ChatClass> usersList;
+    String sName, sDob, sPhone, sEmail, sPassword, sAddress, sWeight, sdesignation = "User", sImageUrl = "null", sDonor = "null", sGender, uID;
+    UsersAdapterClass usersAdapterClass;
+    UsersAdapter usersAdapter;
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    CollectionReference collectionReference;
+    FirebaseUser currentUser;
+
+    ArrayList<UserClass> usersList;
     RecyclerView recyclerView;
     
     @Nullable
@@ -29,28 +49,78 @@ public class ChatFragment extends Fragment {
 
         View view =  inflater.inflate(R.layout.fragment_chat , container ,false);
 
+        usersList = new ArrayList<UserClass>();
+        collectionReference = firebaseFirestore.collection("UserInformation");
+        usersAdapter = new UsersAdapter(usersList, getContext());
+
         recyclerView = view.findViewById(R.id.chat_list_recycler);
 
-        usersList = new ArrayList<ChatClass>();
+        collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
-        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+                usersList.clear();
 
-        ChatAdapter adapter = new ChatAdapter(usersList);
+                for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots)
+                {
+
+                    sName = documentSnapshot.getString("name");
+                    sDob = documentSnapshot.getString("dob");
+                    sPhone = documentSnapshot.getString("phoneNo");
+                    sEmail = documentSnapshot.getString("email");
+//                    sPassword = documentSnapshot.getString("sPassword");
+                    sAddress = documentSnapshot.getString("address");
+                    sWeight = documentSnapshot.getString("weight");
+                    sdesignation = documentSnapshot.getString("designation");
+                    sImageUrl = documentSnapshot.getString("imageURL");
+                    sGender = documentSnapshot.getString("gender");
+                    uID = documentSnapshot.getString("uId");
+                    sDonor = documentSnapshot.getString("donor");
+                    Log.e("Name", sName);
+                    Log.e("UID", uID);
+
+                    if(currentUser != null)
+                    {
+                        if(uID.equalsIgnoreCase(currentUser.getUid()))
+                        {
+                            continue;
+                        }
+                    }
+
+                    usersList.add(new UserClass(sName, sDob, sGender, sPhone, sEmail, sAddress, sWeight, sDonor, sdesignation, uID, sImageUrl));
+//    public UserClass(String name, String dob, String gender, String phoneNo, String email, String address, String weight, String donor, String sdesignation, String uId, String imageURL) {
+
+                }
+
+                usersAdapter.notifyDataSetChanged();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+//        usersList.add(new ChatClass(R.drawable.profile_avatar1,"Sujit Kumar Das", "I want to donate.", "26/01/2020", "7:20 pm"));
+
+
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(usersAdapter);
         
         return view;
     }
